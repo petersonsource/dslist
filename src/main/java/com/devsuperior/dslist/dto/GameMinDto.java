251623d06c12afcfd41b9f1421dcece809da3fe3
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-07-14 14:55:38 -0300

    homolog ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDto.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDto.java
@@ -26,7 +26,7 @@ public class GameMinDto {
     public GameMinDto(GameMinProjection projection) {
         this.id = projection.getId();
         this.title = projection.getTitle();
-        this.year = projection.getYear();
+        this.year = projection.getGameYear();
         this.imgUrl = projection.getImgUrl();
         this.shortDescription = projection.getShortDescription();
     }
